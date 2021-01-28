77e9a7c1abf9c2c2ed9c4db72576994865dae6a2
Collins Thomas <thomascollins582@gmail.com>
2021-01-28 16:51:19 +0100

    Installed dotenv package and moved port number to config file

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const app = require("./app");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 
-const PORT = 3300;
+const PORT = process.env.PORT || 3300;
 
 app.listen(PORT, () => {
   console.log(`App is Running on port ${PORT}`);
