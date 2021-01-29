f14204712179088326aa236a56006bae2b9513b4
Collins Thomas <thomascollins582@gmail.com>
2021-01-29 08:32:50 +0100

    Added Event Listener to listen to unhandled error

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const app = require("./app");
 
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught Exception!! Shutting Down");
+  console.log({ name: err.name, message: err.message });
+  process.exit(1);
+});
+
 const PORT = process.env.PORT || 3300;
 
 app.listen(PORT, () => {
